71bd0b7c192a4782ccd095ec4d6733a0e44a497a
Kendra Morrey <kendramorrey@Kendra-pieFi.local>
2022-02-22 15:17:42 -0800

    Added constructor parameters and additional testing of chunk sizes

--- src/contracts/LookupContract.sol
+++ src/contracts/LookupContract.sol
@@ -17,5 +17,34 @@ function setMobileNumber(string memory _name, uint _mobileNumber) public {
 function getMobileNumber(string memory _name) public view returns (uint) {
         return myDirectory[_name];
     }
+function setMobileNumber1(string memory _name, uint _mobileNumber) public {
+        myDirectory[_name] = _mobileNumber;
+    }
+
+function getMobileNumber1(string memory _name) public view returns (uint) {
+        return myDirectory[_name];
+    }
+
+function setMobileNumber2(string memory _name, uint _mobileNumber) public {
+        myDirectory[_name] = _mobileNumber;
+    }
+
+function getMobileNumber2(string memory _name) public view returns (uint) {
+        return myDirectory[_name];
+    }
+function setMobileNumber3(string memory _name, uint _mobileNumber) public {
+        myDirectory[_name] = _mobileNumber;
+    }
+
+function getMobileNumber3(string memory _name) public view returns (uint) {
+        return myDirectory[_name];
+    }
+function setMobileNumber4(string memory _name, uint _mobileNumber) public {
+        myDirectory[_name] = _mobileNumber;
+    }
+
+function getMobileNumber4(string memory _name) public view returns (uint) {
+        return myDirectory[_name];
+    }
 
 }
--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ import {
 	FileAppendTransaction,
 	ContractCreateTransaction,
 	ContractFunctionParameters,
+	ContractCallQuery,
 } from "@hashgraph/sdk";
 import fs from "fs";
 
@@ -19,7 +20,7 @@ const operatorKey = PrivateKey.fromString(process.env.OPERATOR_PVKEY);
 const client = Client.forTestnet().setOperator(operatorId, operatorKey); // Currently only for testnet
 
 async function main() {
-	const maxFileSize = 1024; // Max file size for Hedera is 1,024 kB
+	const chunkSize = 1024; // Max chunk size (Hedera uploads in chunks of 1kb)
 	const successCode = 22; // A transaction receipt returns a status code of 22 if the transaction was a success
 
 	// Get bin path from .env
@@ -27,8 +28,8 @@ async function main() {
 
 	// Determine size of bin file and chunks
 	const contractBytecodeSizeKB = fs.statSync(process.env.BIN).size;
-	const chunkNum = Math.ceil(contractBytecodeSizeKB / maxFileSize); //TODO - fix math here (right now we're comaring kB to bytes)
-	console.log("Number of chunks is: ", chunkNum);
+	const maxChunks = Math.ceil(contractBytecodeSizeKB / chunkSize) + 1;
+	console.log("Number of chunks is: ", maxChunks);
 
 	console.log("Contract size is: ", contractBytecodeSizeKB);
 
@@ -55,7 +56,7 @@ async function main() {
 		const fileAppendTx = new FileAppendTransaction()
 			.setFileId(bytecodeFileId)
 			.setContents(contractBytecode)
-			.setMaxChunks(chunkNum)
+			.setMaxChunks(maxChunks)
 			.freezeWith(client);
 		const fileAppendSign = await fileAppendTx.sign(operatorKey);
 		const fileAppendSubmit = await fileAppendSign.execute(client);
@@ -70,15 +71,100 @@ async function main() {
 		// Log file append transaction status
 		console.log(`The file append was a : ${fileAppendRx.status}`);
 
-		// Instantiate smart contract (get contructor parameters and gas?)
+		// Parse constructor parameters and create string
+		const parameters = JSON.parse(process.env.CONSTRUCTOR_PARAMS);
+		let constructorParams = new ContractFunctionParameters();
+
+		for (const key in parameters) {
+			const type = parameters[key][0].toLowerCase();
+			const value = parameters[key][1];
+
+			switch (
+				type //Accounting for everything except functions. TODO: add in functions
+			) {
+				case "string":
+					constructorParams.addString(value);
+					break;
+				case "address":
+					constructorParams.addAddress(value);
+					break;
+				case "addressarray":
+					constructorParams.addAddressArray(value);
+					break;
+				case "bool":
+					constructorParams.addBool(value);
+					break;
+				case "bytes":
+					constructorParams.addBytes(value);
+					break;
+				case "bytes32":
+					constructorParams.addBytes32(value);
+					break;
+				case "bytes32array":
+					constructorParams.addBytes32Array(value);
+					break;
+				case "bytesArray":
+					constructorParams.addBytesArray(value);
+					break;
+				case "int256":
+					constructorParams.addInt256(value);
+					break;
+				case "int256array":
+					constructorParams.addInt256Array(value);
+					break;
+				case "int32":
+					constructorParams.addInt32(value);
+					break;
+				case "int32array":
+					constructorParams.addInt32Array(value);
+					break;
+				case "int64":
+					constructorParams.addInt64(value);
+					break;
+				case "int64array":
+					constructorParams.addInt64Array(value);
+					break;
+				case "int8":
+					constructorParams.addInt8(value);
+					break;
+				case "int8array":
+					constructorParams.addInt8Array(value);
+					break;
+				case "stringarray":
+					constructorParams.addStringArray(value);
+					break;
+				case "uint256":
+					constructorParams.addUint256(value);
+					break;
+				case "uint256array":
+					constructorParams.addUint256Array(value);
+					break;
+				case "uint32":
+					constructorParams.addUint32(value);
+					break;
+				case "uint32array":
+					constructorParams.addUint32Array(value);
+					break;
+				case "uint64":
+					constructorParams.addUint64(value);
+					break;
+				case "uint64array":
+					constructorParams.addUint64Array(value);
+					break;
+				case "uint8":
+					constructorParams.addUint8(value);
+					break;
+				case "uint8array":
+					constructorParams.addUint8Array(value);
+					break;
+			}
+		}
+
+		// Instantiate smart contract
 		const contractInstantiateTx = new ContractCreateTransaction()
 			.setBytecodeFileId(bytecodeFileId)
 			.setGas(parseInt(process.env.CONTRACT_GAS))
-			.setConstructorParameters(
-				new ContractFunctionParameters()
-					.addString("Alice")
-					.addUint256(111111)
-			); // TODO: EDIT THIS
+			.setConstructorParameters(constructorParams);
 		const contractInstantiateSubmit = await contractInstantiateTx.execute(
 			client
 		);
@@ -98,6 +184,20 @@ async function main() {
 		console.log(`The smart contract ID is: ${contractId}`);
 		console.log(
 			`The smart contract Solidity address is: ${contractAddress}`
+		);
+
+		// Query the contract to check changes in state variable - JUST FOR TESTING
+		const contractQueryTx = new ContractCallQuery()
+			.setContractId(contractId)
+			.setGas(100000)
+			.setFunction(
+				"getMobileNumber",
+				new ContractFunctionParameters().addString("Alice")
+			);
+		const contractQuerySubmit = await contractQueryTx.execute(client);
+		const contractQueryResult = contractQuerySubmit.getUint256(0);
+		console.log(
+			`- Here's the phone number that you asked for: ${contractQueryResult} \n`
 		);
 	} catch (err) {
 		console.log(err);
